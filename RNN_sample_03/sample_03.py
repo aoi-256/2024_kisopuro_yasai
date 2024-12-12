0bf100dbff9dd272e995e1acf5c09cefb1d38f7d
aoi-256 <aoi256jp@Gmail.com>
2024-12-12 14:37:23 +0900

    RNN_sample_03開発終了（？）

--- RNN_sample_03/sample_03.py
+++ RNN_sample_03/sample_03.py
@@ -109,25 +109,33 @@ if __name__ == '__main__':
 
     """ 産地データの処理 """
 
-    # area列を処理
     area = [elem.split('_') for elem in data['area']]
 
     # 各要素を列として扱うためにデータフレームに変換
     area_data = pd.DataFrame(area)
 
-    # Nanを埋める（もし存在する場合）
+    # データがないところと各地の項を空にする
+    #area_data = area_data.replace('各地', '')
     area_data = area_data.fillna('')
 
-    # OneHotEncoderを使用してエンコード
-    area_encoder = OneHotEncoder(sparse_output=False)
-    area_encoded = area_encoder.fit_transform(area_data.apply(lambda x: ' '.join(x), axis=1).str.get_dummies(sep=' '))
+    # 分割したデータの1番目の要素をdata['area_1']に格納
+    data['area_1'] = area_data[0]
+    data['area_2'] = area_data[1]
+    data['area_3'] = area_data[2]
 
-    # 各要素の配列を出力
-    elements = area_data.values.T.tolist()
+    encoder = OneHotEncoder(sparse_output=False)
 
-    for i, element_list in enumerate(elements, start=1):
+    # area_1のエンコード
+    encoded_area_1 = encoder.fit_transform(data[['area_1']]).astype(int)
+    data['area_1'] = [''.join(map(str, row)) for row in encoded_area_1]
 
-        data['area_' + str(i)] = element_list
+    # area_2のエンコード
+    encoded_area_2 = encoder.fit_transform(data[['area_2']]).astype(int)
+    data['area_2'] = [''.join(map(str, row)) for row in encoded_area_2]
+
+    # area_3のエンコード
+    encoded_area_3 = encoder.fit_transform(data[['area_3']]).astype(int)
+    data['area_3'] = [''.join(map(str, row)) for row in encoded_area_3]
     
     """ 学習データの設定 """
 
--- test2.py
+++ test2.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import OneHotEncoder
 
-# サンプルデータを作成
+# データの読み込み
 data = pd.read_csv('negi_data.csv', encoding='shift_jis')
 
 # area列を処理
@@ -11,11 +11,9 @@ area = [elem.split('_') for elem in data['area']]
 # 各要素を列として扱うためにデータフレームに変換
 area_data = pd.DataFrame(area)
 
-# Nanを埋める（もし存在する場合）
+# '各地'を空文字列に置き換え、Nanを埋める
+area_data = area_data.replace('各地', '')
 area_data = area_data.fillna('')
-
-# 各要素の配列を出力
-elements = area_data.values.T.tolist()
 
 # 分割したデータの1番目の要素をdata['area_1']に格納
 data['area_1'] = area_data[0]
@@ -23,12 +21,18 @@ data['area_2'] = area_data[1]
 data['area_3'] = area_data[2]
 
 encoder = OneHotEncoder(sparse_output=False)
-for col in ['area_1', 'area_2', 'area_3']: 
-    encoded = encoder.fit_transform(data[[col]]) 
-    encoded_df = pd.DataFrame(encoded, columns=[f'{col}_{i}' for i in range(encoded.shape[1])]) 
-    data = pd.concat([data, encoded_df], axis=1)
+
+# area_1のエンコード
+encoded_area_1 = encoder.fit_transform(data[['area_1']]).astype(int)
+data['encoded_area_1'] = [''.join(map(str, row)) for row in encoded_area_1]
+
+# area_2のエンコード
+encoded_area_2 = encoder.fit_transform(data[['area_2']]).astype(int)
+data['encoded_area_2'] = [''.join(map(str, row)) for row in encoded_area_2]
+
+# area_3のエンコード
+encoded_area_3 = encoder.fit_transform(data[['area_3']]).astype(int)
+data['encoded_area_3'] = [''.join(map(str, row)) for row in encoded_area_3]
 
 # 結果を表示
-print(data.head())
-
-
+print(data[['encoded_area_1', 'encoded_area_2', 'encoded_area_3']].head())
